12a4711db87473479f9f1da9a4a06fdceefeb98e
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-06-11 12:15:13 +0200

    fix all courses model query teacher name

--- src/features/courses/course.model.ts
+++ src/features/courses/course.model.ts
@@ -7,10 +7,12 @@ export const selectAll = async (userId: number): Promise<ICourse[]> => {
     c.title,
     c.short_description,
     c.image_url,
-    c.teacher_id,
+    CONCAT(u.first_name, ' ', u.last_name) AS teacher_name,
     'teacher' AS user_role_in_course
 FROM
     courses c
+JOIN
+    users u ON c.teacher_id = u.id
 WHERE
     c.teacher_id = ?
 UNION
@@ -19,14 +21,16 @@ SELECT
     c.title,
     c.short_description,
     c.image_url,
-    c.teacher_id,
+    CONCAT(u.first_name, ' ', u.last_name) AS teacher_name,
     'student' AS user_role_in_course
 FROM
     courses c
 JOIN
     enrollments e ON c.id = e.course_id
+JOIN
+    users u ON c.teacher_id = u.id
 WHERE
-    e.student_id = ?`;
+    e.student_id = ?;`;
   const [rows] = await db.query(query, [userId, userId]);
   return rows as ICourse[];
 };
