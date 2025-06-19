b7cb5a2dfb30b601ab6cfa1c4199b2532a7b9dcc
Pablopserrada <pablopserrada@gmail.com>
2025-06-19 22:34:48 +0200

    Añadidas función edit y delete Response y Thread

--- src/features/forum/forum.controller.ts
+++ src/features/forum/forum.controller.ts
@@ -115,4 +115,128 @@ export const postResponse = async (
   }
 };
 
-export default { getAllThreadsWithRepliesAndUsers, postThread, postResponse };
+export const editThread = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { content, uuid } = req.body as IPostResponsePayload;
+
+    if (!uuid) {
+      res.status(400).json({ error: "Thread UUID requerido" });
+      return;
+    }
+
+    if (!content || typeof content !== "string" || content.trim() === "") {
+      res.status(400).json({ error: "Contenido del thread requerido" });
+      return;
+    }
+
+    await Forum.editThread(uuid, content)
+
+    const newResponseDetails = await Forum.selectThreadByUuid(uuid);
+
+    res.status(201).json(newResponseDetails);
+  } catch (error){
+    console.error("Error en editThread controller:", error);
+    res.status(500).json({
+      status: "error",
+      message: GENERAL_SERVER_ERROR_MESSAGE,
+    });
+  }
+}
+
+export const editResponse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { content, uuid } = req.body as IPostResponsePayload;
+
+    if (!uuid) {
+      res.status(400).json({ error: "Response UUID requerido" });
+      return;
+    }
+
+    if (!content || typeof content !== "string" || content.trim() === "") {
+      res.status(400).json({ error: "Contenido de la response requerido" });
+      return;
+    }
+
+    await Forum.editResponse(uuid, content)
+
+    const newResponseDetails = await Forum.selectResponseByUuid(uuid);
+
+    res.status(201).json(newResponseDetails);
+  } catch (error){
+    console.error("Error en editResponse controller:", error);
+    res.status(500).json({
+      status: "error",
+      message: GENERAL_SERVER_ERROR_MESSAGE,
+    });
+  }
+}
+
+
+
+export const deleteThread = async (
+  req: Request,
+  res: Response
+): Promise <void> => {
+  try {
+    const { threadUuid } = req.params;
+
+    if (!threadUuid) {
+      res.status(400).json({ error: "Thread UUID requerido" });
+      return;
+    }
+
+    if (!Forum.selectThreadByUuid(threadUuid)) {
+      res.status(404).json({ error: "No se encontró un hilo con ese id." });
+      return;
+    }
+
+    await Forum.deleteThread(threadUuid)
+    res.status(201).json({ error: `Se ha eliminado correctamente el hilo con uuid ${threadUuid}`})
+
+  } catch (error){
+    console.error("Error en deleteThread controller:", error);
+    res.status(500).json({
+      status: "error",
+      message: GENERAL_SERVER_ERROR_MESSAGE,
+    });
+  }
+}
+
+export const deleteResponse = async (
+  req: Request,
+  res: Response
+): Promise <void> => {
+  try {
+    const { responseUuid } = req.params;
+
+    if (!responseUuid) {
+      res.status(400).json({ error: "Response UUID requerido" });
+      return;
+    }
+
+    if (!Forum.selectResponseByUuid(responseUuid)) {
+      res.status(404).json({ error: "No se encontró una respuesta con ese id." });
+      return;
+    }
+
+    await Forum.deleteResponse(responseUuid)
+    res.status(201).json({ error: `Se ha eliminado correctamente la respuesta con uuid ${responseUuid}`})
+
+  } catch (error){
+    console.error("Error en deleteResponse controller:", error);
+    res.status(500).json({
+      status: "error",
+      message: GENERAL_SERVER_ERROR_MESSAGE,
+    });
+  }
+}
+
+
+
+export default { getAllThreadsWithRepliesAndUsers, postThread, postResponse, editResponse, deleteResponse };
